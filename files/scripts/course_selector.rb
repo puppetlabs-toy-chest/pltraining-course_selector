6fea7833550fac749ad0997481ea52383c28958f
Josh Samuelson <js@puppetlabs.com>
2015-09-23 11:53:06 -0700

    Fixing update script

--- files/scripts/course_selector.rb
+++ files/scripts/course_selector.rb
@@ -12,7 +12,7 @@ puts "Updating course content, please stand by."
 %x(cd /usr/src/pltraining-course_selector && git pull)
 
 # Update courseware content
-%x(puppet apply /usr/src/pltraining-course_selector/scripts/update.pp)
+%x(puppet apply /usr/src/pltraining-course_selector/files/scripts/update.pp)
 
 # Run course_update script
 %x(/usr/local/bin/course_update)
