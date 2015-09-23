e9e43d376b03e1162c69e67b29b9be9b62c3eecb
Josh Samuelson <js@puppetlabs.com>
2015-09-23 12:12:06 -0700

    Fixed variable

--- manifests/course/infrastructure.pp
+++ manifests/course/infrastructure.pp
@@ -1,6 +1,6 @@
 class course_selector::course::infrastructure {
   include course_selector
-  file {"${course_selector::code_dir}/environments":
+  file {"${course_selector::codedir}/environments":
     ensure  => directory,
     source  => 'puppet:///modules/course_selector/infrastructure/environments',
     recurse => true,
