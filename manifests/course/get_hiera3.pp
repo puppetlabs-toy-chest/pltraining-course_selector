48f183cb1f146ffeca1912bbad7d503d290becc1
Josh Samuelson <js@puppet.com>
2016-09-01 15:14:55 -0700

    Hiera files on the master

--- manifests/course/get_hiera3.pp
+++ manifests/course/get_hiera3.pp
@@ -4,10 +4,25 @@ class course_selector::course::get_hiera3 (
   file { "/etc/puppetlabs/code/hieradata":
     ensure  => directory,
     recurse => true,
+    replace => false,
     source  => "puppet:///modules/course_selector/get_hiera/${unit}/hieradata",
   }
   file { "/etc/puppetlabs/code/hiera.yaml":
-    ensure => file,
-    source => "puppet:///modules/course_selector/get_hiera/${unit}/hiera.yaml",
+    ensure  => file,
+    replace => false,
+    source  => "puppet:///modules/course_selector/get_hiera/${unit}/hiera.yaml",
+  }
+  file { ["/root/puppetcode/hieradata", "/root/puppetcode/hieradata/nodes"]:
+    ensure => directory,
+  }
+  file { "/root/puppetcode/hieradata/common.yaml":
+    ensure  => file,
+    replace => false,
+    source  => "puppet:///modules/course_selector/get_hiera/${unit}/hieradata/common.yaml",
+  }
+  file { "/root/puppetcode/hieradata/nodes/${::fqdn}.yaml":
+    ensure  => file,
+    replace => false,
+    content => '---',
   }
 }
