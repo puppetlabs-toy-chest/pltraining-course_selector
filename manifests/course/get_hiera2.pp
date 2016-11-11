f9b36dc042ef171819ef547b924befd1bed278d8
Josh Samuelson <js@puppet.com>
2016-11-10 17:21:10 -0800

    Use local fact dir

--- manifests/course/get_hiera2.pp
+++ manifests/course/get_hiera2.pp
@@ -6,7 +6,10 @@ class course_selector::course::get_hiera2 (
     recurse => true,
     source  => "puppet:///modules/course_selector/get_hiera/${unit}/hieradata",
   }
-  file {'/opt/puppetlabs/facter/facts.d/my_facts.txt':
+  file {['/etc/puppetlabs/facter','/etc/puppetlabs/facter/facts.d']:
+    ensure => directory,
+  }
+  file {'/etc/puppetlabs/facter/facts.d/my_facts.txt':
     ensure  => file,
     content => 'app_tier=prod',
   }
