9ee20d1c0a6a36594a8fa49e30ab7ab70ecc94cf
Josh Samuelson <js@puppet.com>
2016-12-09 11:47:46 -0800

    Use puppet-nginx instead of deprecated module

--- manifests/course/parser.pp
+++ manifests/course/parser.pp
@@ -4,7 +4,7 @@ class course_selector::course::parser {
     recurse => true,
     source  => 'puppet:///modules/course_selector/parser/example_modules',
   }
-  exec {'puppet module install jfryman-nginx --modulepath=/etc/puppetlabs/code/modules':
+  exec {'puppet module install puppet-nginx --modulepath=/etc/puppetlabs/code/modules':
     path => '/usr/local/bin:/bin'
   }
 }
