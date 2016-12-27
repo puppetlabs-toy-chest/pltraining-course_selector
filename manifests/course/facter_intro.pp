33175174d52c30fed1115cefcf5d9d64a57a7208
josephoaks <majus457@gmail.com>
2016-12-27 10:00:11 -0700

    Fixed syntax

--- manifests/course/facter_intro.pp
+++ manifests/course/facter_intro.pp
@@ -2,7 +2,7 @@ class course_selector::course::facter_intro {
   include course_selector
   file { '/root/puppetcode/modules/motd':
     ensure  => directory,
-    recurse => true
+    recurse => true,
     replace => false,
     source  => 'puppet:///modules/course_selector/facter/motd',
   }
